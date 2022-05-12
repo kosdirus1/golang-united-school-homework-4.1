836491c3bf1eccf93c0c65949920286e86a4fd9a
Ivan <kos.dirus@gmail.com>
2022-05-12 16:56:47 +0200

    finished homework 4.1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,21 @@
 package reverse_string
 
+import "strings"
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+	var res strings.Builder
+	inputLen := len(input)
+	resRunes := make([]rune, inputLen)
+
+	for i, v := range []rune(input) {
+		resRunes[inputLen-1-i] = v
+	}
+
+	for _, v := range resRunes {
+		res.WriteRune(v)
+	}
+
+	output = res.String()
+
 	return output
 }
