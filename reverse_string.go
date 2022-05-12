5881a6f29c97c09998d1637f048eee8bf2685065
Ivan <kos.dirus@gmail.com>
2022-05-12 19:16:18 +0200

    deleted TrimSpace, changed result slice to []string

--- reverse_string.go
+++ reverse_string.go
@@ -7,17 +7,16 @@ import (
 
 func ReverseString(input string) (output string) {
 	var res strings.Builder
-	input = strings.TrimSpace(input)
 	input = emoji.Sprint(input)
 	inputLen := len(input)
-	resRunes := make([]rune, inputLen)
+	resRunes := make([]string, inputLen)
 
 	for i, v := range []rune(input) {
-		resRunes[inputLen-1-i] = v
+		resRunes[inputLen-1-i] = string(v)
 	}
 
 	for _, v := range resRunes {
-		res.WriteRune(v)
+		res.WriteString(v)
 	}
 
 	output = res.String()
